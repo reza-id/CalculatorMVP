31e5af6670fbc43f75f36f635d8ffb762f568f86
Reza Agustiana <reza.agustiana@gmail.com>
2017-12-11 14:21:21 +0700

    first commit

---
+++ app/src/main/java/com/reza/calculator/CalculatorActivity.java
@@ -0,0 +1,12 @@
+package com.reza.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
