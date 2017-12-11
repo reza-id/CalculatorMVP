cca5aaf924d6e12d6bbc199723eb032103ad5820
Reza Agustiana <reza.agustiana@gmail.com>
2017-12-11 20:51:19 +0700

    use fragmentmanager instead

--- app/src/main/java/com/reza/calculator/CalculatorActivity.java
+++ app/src/main/java/com/reza/calculator/CalculatorActivity.java
@@ -1,5 +1,7 @@
 package com.reza.calculator;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,10 +12,15 @@ public class CalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        DisplayFragment displayFragment = (DisplayFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.frag_display);
-        InputFragment inputFragment = (InputFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.frag_input);
+        FragmentManager manager = getSupportFragmentManager();
+
+        DisplayFragment displayFragment = DisplayFragment.newInstance();
+        InputFragment inputFragment = InputFragment.newInstance();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.frag_display, displayFragment);
+        transaction.add(R.id.frag_input, inputFragment);
+        transaction.commit();
 
         // Actually, the presenter should be initialized in the fragment it self, we'll get another example project
         CalculatorPresenter presenter = new CalculatorPresenter(displayFragment);
