07c462ff08e9afea0c35c35b022d042701a3996a
Reza Agustiana <reza.agustiana@gmail.com>
2017-12-11 18:25:07 +0700

    implement presenter

---
+++ app/src/main/java/com/reza/calculator/CalculatorPresenter.java
@@ -0,0 +1,39 @@
+package com.reza.calculator;
+
+/**
+ * Created by reza on 12/11/17.
+ */
+
+public class CalculatorPresenter implements CalculatorMVP.ForwardDisplayInteractionToPresenter,
+        CalculatorMVP.ForwardInputInteractionToPresenter {
+
+    @Override
+    public void onDeleteShortClick() {
+
+    }
+
+    @Override
+    public void onDeleteLongClick() {
+
+    }
+
+    @Override
+    public void onNumberClick(int number) {
+
+    }
+
+    @Override
+    public void onDecimalClick() {
+
+    }
+
+    @Override
+    public void onEvaluateClick() {
+
+    }
+
+    @Override
+    public void onOperatorClick(String operator) {
+
+    }
+}
