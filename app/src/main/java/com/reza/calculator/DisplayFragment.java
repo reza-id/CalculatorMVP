987b4f73d892d9d493d147abc624ce05515ca085
Reza Agustiana <reza.agustiana@gmail.com>
2017-12-11 14:41:07 +0700

    Flexible UI

---
+++ app/src/main/java/com/reza/calculator/DisplayFragment.java
@@ -0,0 +1,23 @@
+package com.reza.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by reza on 12/11/17.
+ */
+
+public class DisplayFragment extends Fragment {
+
+    public DisplayFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_display, container, false);
+    }
+}
